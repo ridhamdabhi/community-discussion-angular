67b1b69cbcf069b60cf65b71b9cbc8628cd2b185
Ridham Nareshbhai Dabhi <rd428850@dal.ca>
2020-09-27 02:39:14 -0300

    Added Project

---
+++ src/app/subreddit/create-subreddit/create-subreddit.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+
+import { CreateSubredditComponent } from "./create-subreddit.component";
+
+describe("CreateSubredditComponent", () => {
+  let component: CreateSubredditComponent;
+  let fixture: ComponentFixture<CreateSubredditComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [CreateSubredditComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CreateSubredditComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});
